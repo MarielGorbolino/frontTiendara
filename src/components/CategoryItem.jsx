30e8038d118db375d6476f101fc190cdd056ec9c
MarielGorbolino <mgorbolino@gmail.com>
2025-11-27 11:45:04 -0300

    fix en los nombres de las categorias

--- src/components/CategoryItem.jsx
+++ src/components/CategoryItem.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 function CategoryItem({ category }) {
   return (
     <div className="overflow-hidden h-80 w-full rounded-2xl group relative">
-      <Link to={"/category/" + (category.name).replace(" ","")}>
+      <Link to={"/category/" + encodeURIComponent(category.name)}>
         <div className="w-full h-full cursor-pointer">
           <div className="absolute inset-0 bg-linear-to-b from-transparent to-gray-900 opacity-50 z-10">
             <img src={category.image} alt={category.name} className="w-full h-full object-cover"/>
